eae9b79c2e0869b27efc4c1a83672ea92b9e5f43
Vamsy <vamsydvjk@gmail.com>
2019-12-25 18:55:17 +0530

    Anime Downloader

--- jsonConverter.py
+++ jsonConverter.py
@@ -9,5 +9,6 @@ from downloader import downloader
 
 url = "http://103.91.144.230/ftpdata/Movies/Hollywood/1900_1999/Billy%20Madison%20%281995%29/Billy.Madison.1995.BluRay.720p.x264.YIFY.mkv"
 output = "F:\\Billy Madison.mp4"
+prin = "Hell"
 
 downloader(url, output)
